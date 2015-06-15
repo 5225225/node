73084f8f73e3dd8294127440445c73477ce9561d
5225225 <5225225@users.noreply.github.com>
2015-06-15 18:14:03 +0100

    Strip out nearly all debug prints.
    The prints were VERY verbose, and stripping them out allows me to make them
    better when I redo them.

--- node.py
+++ node.py
@@ -13,6 +13,8 @@ assert len(PROTOCOL_VERSION) == 16
 
 PORT = 3514
 
+VERBOSE = True
+
 known_messages = {}
 
 
@@ -92,6 +94,7 @@ def sync(ip, port=3514):
     print("Connected to server")
     print("Server version: {}".format(serverver.replace(b"\x00", b"")))
     print("Client version: {}".format(PROTOCOL_VERSION.replace(b"\x00", b"")))
+
     if serverver != PROTOCOL_VERSION:
         print("Hold on, that's not the right version... Disconnect!")
         s.shutdown(socket.SHUT_RDWR)
@@ -99,26 +102,14 @@ def sync(ip, port=3514):
         return
 
     known_ids = [x for x in known_messages]
-    print("I know of {} message, their ID's are below.".format(len(known_ids)))
-    for x in known_ids:
-        print(hex(int.from_bytes(x, "big")))
     ids = b"".join(known_ids)
 
     lenids = int.to_bytes(len(known_ids), 2, "big")
 
-    print("Going to send the length of the known IDS")
-    print("As an integer: {}".format(len(known_ids)))
-    print("As bytes: {}".format(lenids))
-    print(lenids)
-
     s.send(lenids)
-
-    print("Going to send following data to server")
-    print(ids)
 
     s.sendall(ids)
 
-    print("Known ID's to the server sent")
 
     data = b""
 
@@ -128,23 +119,10 @@ def sync(ip, port=3514):
 
     for _ in range(server_lenids):
         nextid = s.recv(32)
-        print("got an ID, {}".format(nextid))
         server_known_ids.append(nextid)
-
-    print(server_known_ids)
-
-    print("-"*30)
-    print("Okay, both the client and the server know what they need to send")
-    print("-"*30)
 
     client_ids = set(known_ids)
     server_ids = set(server_known_ids)
-
-    print("I, the client know of {} ID's.".format(len(client_ids)))
-    print("They are below")
-    print()
-    for x in client_ids:
-        print(x)
 
     tosend = client_ids - server_ids
     torecv = server_ids - client_ids
@@ -155,85 +133,41 @@ def sync(ip, port=3514):
         s.close()
         return
 
-    print("I need to send {} messages to the server".format(len(tosend)))
-    print("They are below")
-    print()
-    for x in tosend:
-        print(x)
-
     for msg in tosend:
         msg = known_messages[msg].serialise()
         s.send(int.to_bytes(len(msg), 8, "big"))
         s.sendall(msg)
 
-    print("I am going to be sent {} messages, their ID's:".format(len(torecv)))
-    for x in torecv:
-        print(x)
-    print()
-
     for _ in range(len(torecv)):
         msglen = int.from_bytes(s.recv(8), "big")
-        print("I will be sent a message of len {}".format(msglen))
         msg = s.recv(msglen)
-        print("Just got a message, printing it out now")
-        print(msg)
-        print("Creating message object")
         newmsg = message.from_serialised(msg)
-        print(newmsg)
-        print("Adding new message object to the set of known messages")
         known_messages[newmsg.msgid] = newmsg
-
-    print("Okay, I'm done getting the server's messages!")
-
-    print("I know of {} messages, they are below".format(len(known_messages)))
-    for x in known_messages:
-        print(known_messages[x])
 
 
 def listen(socket):
     conn, addr = s.accept()
-    print("I'm really copying this from the tutorial.")
-    print("Anyway, {} wanted to say hello.".format(addr))
     clientver = conn.recv(16)
-    print("Client is running version {}".format(clientver))
     if clientver != PROTOCOL_VERSION:
-        print("That's not the right version!")
-        print("Send our version anyway, but expect a disconnect once we do")
         conn.send(PROTOCOL_VERSION)
         conn.close()
         return
 
     conn.send(PROTOCOL_VERSION)
-    print("Version sent")
 
     client_known_ids = []
 
     client_lenids_bytes = conn.recv(2)
     client_lenids = int.from_bytes(client_lenids_bytes, "big")
-    print("Recieved how many ID's I am about to be sent")
-    print("As bytes: {}".format(client_lenids_bytes))
-    print("As an integer: {}".format(client_lenids))
 
 
-    print(client_lenids)
     for _ in range(client_lenids):
-        print(_)
         nextid = conn.recv(32)
-        print("got an ID, {}".format(nextid))
         client_known_ids.append(nextid)
-
-    print("The client knows of {} messages, their ID's:".format(
-        len(client_known_ids)))
-
-    for x in client_known_ids:
-        print(b"    " + x)
 
     # Now do it again, in reverse
 
     known_ids = [x for x in known_messages]
-    print("I know of {} message, their ID's are below.".format(len(known_ids)))
-    for x in known_ids:
-        print(b"    " + x)
 
     lenids = len(known_ids)
     lenids_bytes = int.to_bytes(lenids, 2, "big")
@@ -242,18 +176,9 @@ def listen(socket):
     tosend = b"".join(known_ids)
     conn.sendall(tosend)
 
-    print("-"*30)
-    print("Okay, both the client and the server know what they need to send")
-    print("-"*30)
 
     server_ids = set(known_ids)
     client_ids = set(client_known_ids)
-
-    print("I, the server know of {} ID's.".format(len(server_ids)))
-    print("They are below")
-    print()
-    for x in server_ids:
-        print(x)
 
     tosend = server_ids - client_ids
     torecv = client_ids - server_ids
@@ -263,34 +188,11 @@ def listen(socket):
         conn.close()
         return
 
-    print("I need to send {} ID's to the client".format(len(tosend)))
-    print("They are below")
-    for x in tosend:
-        print(x)
-    print()
-
-    print("I am going to be SENT {} messages, their ID's:".format(len(torecv)))
-    for x in torecv:
-        print(x)
-    print()
-
     for _ in range(len(torecv)):
         msglen = int.from_bytes(conn.recv(8), "big")
-        print("I will be sent a message of len {}".format(msglen))
         msg = conn.recv(msglen)
-        print("Just got a message, printing it out now")
-        print(msg)
-        print("Creating message object")
         newmsg = message.from_serialised(msg)
-        print(newmsg)
-        print("Adding new message object to the set of known messages")
         known_messages[newmsg.msgid] = newmsg
-
-    print("Okay, I'm done recieving the client's messages!")
-
-    print("I know of {} messages, they are below".format(len(known_messages)))
-    for x in known_messages:
-        print(known_messages[x])
 
     # Now I copy paste and hope for the best!
 
@@ -298,13 +200,6 @@ def listen(socket):
         tosend = known_messages[msg].serialise()
         conn.send(int.to_bytes(len(tosend), 8, "big"))
         conn.sendall(tosend)
-
-    print("Sync finished!")
-    print()
-    print()
-    print()
-    print()
-
 
 if sys.argv[1] == "SELFTEST":
     with open("test", "rb") as f:
