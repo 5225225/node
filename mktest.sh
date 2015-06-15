a82a8f708ae33275785450e65e24536c08748e5e
5225225 <5225225@users.noreply.github.com>
2015-06-15 18:13:39 +0100

    Fix directories where test data is generated

--- mktest.sh
+++ mktest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-mkdir both client test
+mkdir both client server
 echo $RANDOM | gpg --encrypt > both/test
 echo $RANDOM | gpg --encrypt > client/test
 echo $RANDOM | gpg --encrypt > server/test
