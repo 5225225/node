ad906b5b1abef57545a1654c67966060eb533898
5225225 <5225225@users.noreply.github.com>
2015-06-18 21:56:55 +0100

    Unify host list for syncd
    Right now, it's 240*rand(.5,1.5) delay, and it uses a tetris-like host picker.
    
    That is, it randomises the list of known hosts, then does each one with the
    delay between hosts. This means that every host on the list will be synced
    with, but the order changes.

--- syncd.py
+++ syncd.py
@@ -3,17 +3,26 @@ import message
 import socket
 import client
 import random
+import time
 
-lan_hosts = {}
-wan_hosts = {}
+hosts = []
 
 if config.CREATE_BROADCASTS:
     print("Going to try to discover LAN hosts")
-    lan_hosts = client.discover_hosts()
-    print("I found {} hosts".format(len(lan_hosts)))
-    print("Printing them below")
-    for host in lan_hosts:
-        print(host)
+    for host in client.discover_hosts():
+        hosts.append(host)
 else:
     print("Not going to try to discover LAN hosts.")
     print("If you want to enable this, set CREATE_BROADCASTS in config.py")
+
+with open("known_hosts") as f:
+    for host in f.readlines():
+        hosts.append(host.strip())
+
+while True:
+    random.shuffle(hosts)
+    for item in hosts:
+        print(item)
+        client.sync(item)
+        time.sleep(.5)
+#        time.sleep(60*random.uniform(5,1.5))
